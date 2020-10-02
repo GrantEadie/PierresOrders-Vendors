b518b956e7327be2795d170050c28baf238e1628
Grant Eadie <grantleadie@gmail.com>
2020-10-02 10:38:02 -0700

    testing dictionary for diplaying orders from vendors.

--- BakerySupply/Controllers/VendorsController.cs
+++ BakerySupply/Controllers/VendorsController.cs
@@ -45,8 +45,8 @@ namespace BakerySupply.Controllers
       Order newOrder = new Order(orderItem);
       foundVendor.AddOrder(newOrder);
       List<Order> vendorOrders = foundVendor.Orders;
-      model.Add("Orders", vendorOrders);
-      model.Add("Vendor", foundVendor);
+      model.Add("orders", vendorOrders);
+      model.Add("vendor", foundVendor);
       return View("Show", model);
     }
   }
